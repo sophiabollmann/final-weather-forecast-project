0a112625778515fa206d5c0aeaa040c7dbf63033
sophiabollmann <sophiabollmann00@gmail.com>
2023-12-10 12:59:38 -0300

    Inject the forecast HTML form Java Script
    It still needs grid fixing

--- src/app.js
+++ src/app.js
@@ -49,7 +49,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` 
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon"> <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png" alt=""  width="62" /></div>
+        <div class="weather-forecast-temperatures"> 
+          <span class="weather-forecast-temperature-max">18º </span>
+          <span class="weather-forecast-temperature-min"> 12º</span>
+        </div>
+      </div>
+    `;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("São Paulo");
+displayForecast();
