4efd6d2e52f38d9e68e29be77f1b44307ba401d6
sophiabollmann <sophiabollmann00@gmail.com>
2023-12-09 23:41:49 -0300

    Typo correction

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  return `${day}, ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
